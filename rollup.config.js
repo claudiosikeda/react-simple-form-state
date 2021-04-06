6c1bab0744b8ce96358d10e887d3e63f0a46998f
Claudio Ikeda <claudiosikeda@gmail.com>
2021-04-06 16:02:48 -0300

    feat(baseProxy): removed baseProxy

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,23 @@
+import fs from 'fs';
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
-import external from 'rollup-plugin-peer-deps-external';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+
+const pkg = JSON.parse(fs.readFileSync('./package.json'))
 
 export default {
   input: 'src/index.js',
   output: {
     file: 'dist/main.js',
     format: 'cjs',
-    sourcemap: true
+    sourcemap: true,
   },
-  external: ['react'],
+  external: Object.keys(pkg.devDependencies),
   plugins: [
-    external(),
+    peerDepsExternal(),
     babel({ exclude: 'node_modules/**' }),
     resolve(),
-    commonjs()
+    commonjs(),
   ],
 };
